038cefaddfb7a4c99b557a6f457056a603ecc1a0
maryammohammedsubhi <109868019+maryammohammedsubhi@users.noreply.github.com>
2022-08-13 16:50:35 +0300

    cw6

---
+++ script.js
@@ -0,0 +1,22 @@
+ alert("مرحبا بكم في موقعي الذي سيحسب درجتك")
+
+ let grade = prompt("اكتب درجتك بالأرقام")
+
+ console.log(grade);
+
+ if(grade >= 90){
+    console.log("لقد حصلت على امتياز 🥳");
+ } else if ("grade >= 80"){
+    console.log("لقد حصلت على جيد جداً🤩");
+ }else if (grade >= 70){
+    console.log("لقد حصلت على جيد🙂");
+ } else if (grade >= 60){
+    console.log("لقد حصلت على مقبول😕");
+ }  else if (grade >= 50){
+    conseol.log("لقد حصلت على ضعيف☹️");
+ }   else { conseol.log ("راسب💔");
+}
+
+ 
+    
+ 
